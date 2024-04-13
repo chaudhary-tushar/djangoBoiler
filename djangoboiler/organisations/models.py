c1395ef0a8124f3c58cbee024d4069d468dd4bb4
Tushar Chaudhary <33207752+chaudhary-tushar@users.noreply.github.com>
2024-04-13 23:11:34 +0000

    Pending changes exported from your codespace

--- djangoboiler/organisations/models.py
+++ djangoboiler/organisations/models.py
@@ -1,1 +1,12 @@
+from django.db import models
+from djangoboiler.utils.models import BaseModel
+
 # Create your models here.
+
+class Organisation(BaseModel):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    logo = models.ImageField(upload_to="organisations/logos", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
