dec53002a0ca744ede48330f494d4a839ad43bfa
spindera <chaudharytushar271@gmail.com>
2024-04-14 04:44:51 +0530

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -83,7 +83,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "djangoboiler.users",
     # Your stuff: custom apps go here
-    "djangoboiler."
+    "djangoboiler.organisations"
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
